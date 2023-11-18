f971a86254fcca8cc32289201b28b11abc067873
Dev <anuragdeep.xon@gmail.com>
2023-11-18 18:44:02 +0530

    build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextBuildId = require("next-build-id");
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  useFileSystemPublicRoutes: true,
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
+};
+
+module.exports = nextConfig;
